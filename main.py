5a03320ed7066565af0d30ffe294441181b24427
G00Z_G00Z <edygomezgg4@gmail.com>
2022-08-22 16:40:31 -0500

    Generated figure

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-thetha = np.arange(0, 2 * np.pi , 2 * np.pi / 100)
+theta = np.arange(0, 2 * np.pi , 2 * np.pi / 100)
+y = np.sin(theta)
+
+plt.plot(theta,y)
+plt.grid(True)
+
+# plt.show() doesnt display the image. Maybe a different port??
+# plt.show()
+
+# Workaround to save the plot into  an image
+plt.savefig("figura")
 
 
-
-
-print(thetha)
