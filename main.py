f30e8506ebe4ef870ccca7e3c6bf29a9ced2a84f
G00Z_G00Z <edygomezgg4@gmail.com>
2022-08-22 16:54:52 -0500

    Terminado el codigo de la clase

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Codigo de la clase
 theta = np.arange(0, 2 * np.pi , 2 * np.pi / 100)
 y = np.sin(theta)
-
-plt.plot(theta,y)
+y2 = np.cos(theta)
+plt.plot(theta,y, "*r", linewidth=5)
+plt.plot(theta,y2, "ob", linewidth=10)
 plt.grid(True)
+plt.title("Seno de un angulo")
+plt.legend([r"sin($\theta$)", r"cos($\theta$)"])
+plt.ylabel(r"y = sin($\theta$)")
+plt.xlabel(r"$\theta$")
 
 # plt.show() doesnt display the image. Maybe a different port??
-# plt.show()
+plt.show()
 
 # Workaround to save the plot into  an image
 plt.savefig("figura")
